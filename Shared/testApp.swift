aff2f1d09ad7d513976d8bc553120797fcb8e7e2
Adam Roberts <Adam_roberts@gmx.com>
2022-03-09 12:02:33 -0500

    Initial Commit

---
+++ Shared/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  Shared
+//
+//  Created by Adam Roberts on 3/9/22.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
